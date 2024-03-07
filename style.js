bf3ff7ddf8f7f5dabfba8621948d1f23a6d6517d
lucabar21 <luca_barbuto@yahoo.it>
2024-03-07 14:55:13 +0100

    merge all the pages and use a singluar style.css and style.js

--- style.js
+++ style.js
@@ -1,3 +1,79 @@
+// GENERAL SCRIPT
+
+// AUDIO CONTROLS
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controls = document.getElementById("ctrl-play");
+let currentTimeDisplay = document.querySelector("#middle-bot small:first-child");
+let durationDisplay = document.querySelector("#middle-bot small:last-child");
+const volume = document.getElementById("volumeRange");
+
+song.onloadedmetadata = () => {
+  progress.max = song.duration;
+  durationDisplay.textContent = formatTime(song.duration);
+};
+// GESTIONE PULSANTE PLAY/PAUSE
+function playPause() {
+  if (controls.classList.contains("bi-pause-fill")) {
+    song.pause();
+    controls.classList.remove("bi-pause-fill");
+    controls.classList.add("bi-play-fill");
+  } else {
+    song.play();
+    controls.classList.remove("bi-play-fill");
+    controls.classList.add("bi-pause-fill");
+  }
+}
+// GESTIONE PROGRESS BAR IN FUNZIONE DELLA TRACCIA AUDIO
+song.addEventListener("timeupdate", () => {
+  progress.value = song.currentTime;
+  currentTimeDisplay.textContent = formatTime(song.currentTime);
+});
+
+// GESTIONE PROGRESS BAR PER IL CLICK DI AVANZAMENTO NELLA TIMELINE DELLA TRACCIA
+progress.oninput = () => {
+  song.currentTime = progress.value;
+  progress.classList.add("slider-moved");
+};
+
+// GESTIONE DEI TESTI DI DURATA AD INIZIO E FINE PROGRESS BAR
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+// GESTIONE BARRA DI VOLUME
+
+volume.addEventListener("input", function () {
+  song.volume = parseFloat(this.value);
+});
+song.addEventListener("volumechange", function () {
+  volume.value = this.volume;
+});
+
+// EVENTO ONCLICK DEL CUORE
+document.getElementById("heart").addEventListener("click", function () {
+  this.classList.toggle("filled"); // Aggiunge o rimuove la classe 'filled'
+});
+
+// GESTIONE DEL DROPDOWN DELLA SIDEBAR
+
+function dropdownBTN() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+      }
+    }
+  }
+};
+
 // SEARCH PAGE REQUEST
 const apiKey = "1e76c6751dmshf6e51120c249691p17f828jsn056e3af8a78c";
 const apiHost = "deezerdevs-deezer.p.rapidapi.com";
